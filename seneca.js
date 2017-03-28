a106ee3d73055b3aceb3812dac0edf5fca81ef05
Oleg Mytryniuk <Oleg Mytryniuk>
2017-03-27 21:02:22 -0400

    JUnit Test updated

--- seneca.js
+++ seneca.js
@@ -3,14 +3,17 @@
 
 /**
  * Given a string `email`, return `true` if the string is in the form
- * of a valid Seneca College email address, `false` othewise.
+ * of a valid Seneca College email address, `false` otherwise.
  */
 exports.isValidEmail = function (email) {
-    // Here is the regexp that must be checked
-  const senecaEmailFormat = /^[a-z]{3,}[0-9]{0,3}@myseneca.ca$/;
+  // Here is the regexp that must be checked
+  const senecaEmailFormat = /^[a-z]{2,}\.?[a-z]{3,}[0-9]{0,3}@(myseneca|senecacollege|senecac\.on).ca$/;
   // var senecaEmailFormat = /@myseneca.ca$/;
-  if (senecaEmailFormat.test(email)) {
-    return true;
+  if (typeof email === 'string') {
+    if (senecaEmailFormat.test(email)) {
+      return true;
+    }
+    return false;
   }
   return false;
 };
@@ -20,6 +23,13 @@ exports.isValidEmail = function (email) {
  * this person. NOTE: the email doesn't need to be real/valid/active.
  */
 exports.formatSenecaEmail = function (name) {
-  const completeName = `${name}@myseneca.ca`;
-  return completeName;
+  const userNameFormat = /^[a-z]{2,}\.?[a-z]{3,}[0-9]{0,3}$/;
+  if (name && typeof name === 'string') {
+    if (userNameFormat.test(name)) {
+      const completeName = `${name}@myseneca.ca`;
+      return completeName;
+    }
+    return false;
+  }
+  return false;
 };
--- seneca.test.js
+++ seneca.test.js
@@ -1,6 +1,76 @@
+// First require (e.g., load) our seneca.js module
 var seneca = require('./seneca');
 
-test('isValid returns true for simple myseneca address', function() {
-  var simpleEmail = 'someone@myseneca.ca';
-  expect(seneca.isValidEmail(simpleEmail)).toBe(true);
+/**
+ * Tests for seneca.isValidEmail()
+ */
+describe('seneca.isValidEmail()', function() {
+
+  test('returns true for simple myseneca address', function() {
+    var simpleEmail = 'someone@myseneca.ca';
+    expect(seneca.isValidEmail(simpleEmail)).toBe(true);
+  });
+
+  test('returns false for a non-myseneca address', function() {
+    var gmailAddress = 'someone@gmail.com';
+    expect(seneca.isValidEmail(gmailAddress)).toBe(false);
+  });
+  
+  test('returns false for an argument that is not a string (int)', function() {
+    var number = 123;
+    expect(seneca.isValidEmail(number)).toBe(false);
+  });
+  
+  test('returns false for an argument that is null', function() {
+    var nullvalue = null;
+    expect(seneca.isValidEmail(nullvalue)).toBe(false);
+  });
+  
+  test('returns email address contains leading whitespace', function() {
+    var leadingSpaceEmail = ' mshaw@myseneca.ca';
+    expect(seneca.isValidEmail(leadingSpaceEmail)).toBe(false);
+  });
+  
+  test('returns email address for a professor', function() {
+    var professorEmail = 'peter.james@senecacollege.ca';
+    expect(seneca.isValidEmail(professorEmail)).toBe(true);
+  });
+  
+  test('returns old style Seneca email address', function() {
+    var oldProfessorEmail = 'peter.james@senecac.on.ca';
+    expect(seneca.isValidEmail(oldProfessorEmail)).toBe(true);
+  });
+  
+});
+
+/**
+ * Tests for seneca.formatSenecaEmail()
+ */
+describe('seneca.formatSenecaEmail()', function() {
+
+  test('adds @myseneca.ca to the end of name', function() {
+    var name = "mshaw";
+    expect(seneca.formatSenecaEmail(name)).toBe('mshaw@myseneca.ca');
+  });
+  
+  test('passes null as a parameter', function() {
+    var name = null;
+    expect(seneca.formatSenecaEmail(name)).toBe(false);
+  });
+  
+  test('passes special characters as a part of the name', function() {
+    var name = "james?anderson";
+    expect(seneca.formatSenecaEmail(name)).toBe(false);
+  });
+  
+  test('passes a name with a leading space', function() {
+    var name = " janderson";
+    expect(seneca.formatSenecaEmail(name)).toBe(false);
+  });
+  
+  test('passes an integer instead of string value', function() {
+    var name = 123;
+    expect(seneca.formatSenecaEmail(name)).toBe(false);
+  });
+
 });
