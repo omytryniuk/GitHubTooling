20b5089f0073547e06c151d96971b268f4e64861
Oleg Mytryniuk <Oleg Mytryniuk>
2017-03-31 13:23:33 -0400

    Add healthcheckroute

--- server.js
+++ server.js
@@ -16,6 +16,27 @@ app.get('/', function (req, res) {
   res.send('My Server is working!');
 });
 
+app.get('/healthcheck', function (req, res) {
+ var time = process.uptime();
+ var updatedtime = (time + "").toHHMMSS();
+ res.json({
+    uptime: updatedtime,
+  });
+});
+
+String.prototype.toHHMMSS = function () {
+    var sec_num = parseInt(this, 10); // don't forget the second param
+    var hours   = Math.floor(sec_num / 3600);
+    var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+    var seconds = sec_num - (hours * 3600) - (minutes * 60);
+
+    if (hours   < 10) {hours   = "0"+hours;}
+    if (minutes < 10) {minutes = "0"+minutes;}
+    if (seconds < 10) {seconds = "0"+seconds;}
+    var time    = hours+':'+minutes+':'+seconds;
+    return time;
+}
+
 // GET http://localhost:3000/validate/someone@myseneca.ca
 app.get('/validate/:email', function (req, res) {
   var email = req.params.email;
